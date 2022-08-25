9ca4ba63dcb454f56226e94e0fdcfc961e3ee9ca
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-08-24 20:49:33 -0600

    Create test for takeTurn function and pass them

--- src/round.js
+++ src/round.js
@@ -1,5 +1,33 @@
+const Turn = require('../src/turns');
+const Deck = require('../src/deck');
+
 class Round {
-    constructor() {
+    constructor(deck) {
+        this.deck = deck
+        this.currentCard = deck.cards[0]
+        this.turnCount = 0
+        this.incorrectGuesses = []
+    }
+    returnCurrentCard() {
+        return this.currentCard
+    }
+    takeTurn(guess) {
+        this.turn = new Turn(guess, this.currentCard)
+        this.turnCount++
+        
+        if(this.turn.guess !== this.currentCard.correctAnswer) {
+            this.incorrectGuesses.push(this.currentCard.id)
+            console.log(this.incorrectGuesses)
+        }
+        this.deck.cards.shift()
+        return this.turn.giveFeedback()
+        
+
+
+
+
+
+
 
     }
 }
