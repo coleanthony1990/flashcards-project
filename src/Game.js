317c660bc59be0b4690a62989afd3e644f96eff1
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-08-25 15:07:59 -0600

    repair round class to accept a new deck instantiation from game class and create start tests and pass

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,21 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card')
+const Round = require('../src/round')
+
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
+  start() {
+    this.cards = prototypeQuestions.map(card => {
+      return card = new Card(card.id, card.question, card.answers, card.correctAnswer);
+    });
+    this.currentRound = new Round(this.cards);
+    
+  };
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
